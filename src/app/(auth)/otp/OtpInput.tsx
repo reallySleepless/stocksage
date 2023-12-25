7dbbcc40ebaadf60c5863023aca1d56efcf33361
himanshuSri24 <himanshu20032002@gmail.com>
2023-12-25 11:12:24 +0530

    Fixes to code

--- src/app/(auth)/otp/OtpInput.tsx
+++ src/app/(auth)/otp/OtpInput.tsx
@@ -1,87 +1,89 @@
 "use client";
-import React from "react";
+import React, { ReactElement } from "react";
 import { Button } from "@components/ui/button";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 const OtpInput = () => {
-	const inputRefs = useRef<any[]>([]);
+  const inputRefs = useRef<any[]>([]);
 
-	inputRefs.current = Array.from(Array(4).keys()).map(
-		(ref, index) => (inputRefs.current[index] = React.createRef())
-	);
+  inputRefs.current = Array.from(Array(4).keys()).map(
+    (ref, index) => (inputRefs.current[index] = React.createRef())
+  );
 
-	useEffect(() => {
-		inputRefs.current[0].current!.focus();
-	}, []);
+  useEffect(() => {
+    inputRefs.current[0].current!.focus();
+  }, []);
 
-	const handleInputChange = (index: number, value: string) => {
-		const regex = /^[0-9\b]+$/;
+  const handleInputChange = (index: number, value: string) => {
+    const regex = /^[0-9\b]+$/;
 
-		if (!regex.test(value)) {
-			inputRefs.current[index].current!.value = "";
-			return;
-		}
+    if (!regex.test(value)) {
+      inputRefs.current[index].current!.value = "";
+      return;
+    }
 
-		const newValue = value.slice(0, 1);
+    const newValue = value.slice(0, 1);
 
-		if (newValue !== value) {
-			inputRefs.current[index].current!.value = newValue;
-		}
+    if (newValue !== value) {
+      inputRefs.current[index].current!.value = newValue;
+    }
 
-		if (newValue && index < inputRefs.current.length - 1) {
-			inputRefs.current[index + 1].current!.focus();
-		}
-	};
+    if (newValue && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].current!.focus();
+    }
+  };
 
-	const handleInputKeyDown = (
-		index: number,
-		e: React.KeyboardEvent<HTMLInputElement>
-	) => {
-		if (
-			e.key === "Backspace" &&
-			index > 0 &&
-			!inputRefs.current[index].current!.value
-		) {
-			inputRefs.current[index - 1].current!.focus();
-		}
-	};
+  const handleInputKeyDown = (
+    index: number,
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (
+      e.key === "Backspace" &&
+      index > 0 &&
+      !inputRefs.current[index].current!.value
+    ) {
+      inputRefs.current[index - 1].current!.focus();
+    }
+  };
 
-	const handleSubmit = () => {
-		const otp = inputRefs.current.map((ref) => ref.current!.value).join("");
-		console.log(otp);
-		window.location.href = "/signup";
-	};
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const otp = inputRefs.current.map((ref) => ref.current!.value).join("");
+    console.log(otp);
+    // TODO: OTP SERVICE
+    window.location.href = "/dashboard";
+  };
 
-	return (
-		<form action="" className={`mt-[50px] w-[500px]`} onSubmit={handleSubmit}>
-			<div className="flex justify-between">
-				{inputRefs.current.map((ref, index) => (
-					<input
-						key={index}
-						ref={ref}
-						type="text"
-						className="w-[78px] h-[78px] text-[24px] text-center bg-[#1d1a3999] rounded-sm"
-						onChange={(e) => handleInputChange(index, e.target.value)}
-						onKeyDown={(e) => handleInputKeyDown(index, e)}
-						max="1"
-					/>
-				))}
-			</div>
-			<Button
-				type="submit"
-				className="w-full mt-7 h-[60px] text-[24px] font-light bg-[#1D1A39]"
-			>
-				Sign In
-			</Button>
-			<p className="text-center mt-3 text-[#5A5A5A] text-[20px]">
-				Didn&apos;t receive code?{" "}
-				<Link className="text-[#383737] text-[20px]" href="">
-					Re-send
-				</Link>
-			</p>
-		</form>
-	);
+  return (
+    <form action="" className={`mt-[50px] w-[500px]`} onSubmit={handleSubmit}>
+      <div className="flex justify-between">
+        {inputRefs.current.map((ref, index) => (
+          <input
+            key={index}
+            ref={ref}
+            type="text"
+            className="w-[78px] h-[78px] text-[24px] text-center bg-[#1d1a3999] rounded-sm"
+            onChange={(e) => handleInputChange(index, e.target.value)}
+            onKeyDown={(e) => handleInputKeyDown(index, e)}
+            max="1"
+          />
+        ))}
+      </div>
+      <Button
+        type="submit"
+        className="w-full mt-7 h-[60px] text-[24px] font-light bg-[#1D1A39]"
+      >
+        Sign In
+      </Button>
+      <p className="text-center mt-3 text-[#5A5A5A] text-[20px]">
+        Didn&apos;t receive code?{" "}
+        <Link className="text-[#383737] text-[20px]" href="#">
+          Re-send
+        </Link>
+      </p>
+    </form>
+  );
 };
 
 export default OtpInput;
--- src/assets/icons/DownArrow.tsx
+++ src/assets/icons/DownArrow.tsx
@@ -1,42 +1,42 @@
 const DownArrow = () => {
-	return (
-		<>
-			<div className="p-2 group grid" style={{ clipPath: "inset(0 0 0 0 )" }}>
-				<div className="[grid-area:1/1] flex items-center justify-center h-10 w-10 transition ease-in-out group-hover:delay-30 -translate-y-10 group-hover:translate-y-0 ">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						className="lucide lucide-chevron-down"
-					>
-						<path d="m6 9 6 6 6-6" />
-					</svg>
-				</div>
-				<div className="[grid-area:1/1] flex items-center justify-center h-10 w-10 transition ease-in-out delay-30 duration-300 group-hover:translate-y-10">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						className="lucide lucide-chevron-down"
-					>
-						<path d="m6 9 6 6 6-6" />
-					</svg>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="p-2 group grid" style={{ clipPath: "inset(0 0 0 0 )" }}>
+        <div className="[grid-area:1/1] flex items-center justify-center h-10 w-10 transition ease-in-out group-hover:delay-30 -translate-y-10 group-hover:translate-y-0 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-chevron-down"
+          >
+            <path d="m6 9 6 6 6-6" />
+          </svg>
+        </div>
+        <div className="[grid-area:1/1] flex items-center justify-center h-10 w-10 transition ease-in-out delay-30 duration-300 group-hover:translate-y-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-chevron-down"
+          >
+            <path d="m6 9 6 6 6-6" />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DownArrow;
