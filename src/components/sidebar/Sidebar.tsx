bc5fe11d704a665bef0be01e36b87d1a7329e271
HarshitTib <harshittibrewal11@gmail.com>
2024-02-24 16:49:33 +0530

    [Done with the Sign up]

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
-import logo from "@assets/logo.png";
+import logo from "@assets/logo.svg";
 import SidebarButton from "./SidebarButton";
 
 const Sidebar = () => {
-  return (
-    <div className="bg-secondary-background p-2 pl-4 flex flex-col items-center gap-4 h-screen">
-      <Image src={logo} alt="StockSage" height={90} className="mb-10" />
-      <SidebarButton text="Dashboard" href="/dashboard" />
-      <SidebarButton text={`My\u00A0Stocks`} href="/myStocks" />
-      <SidebarButton text={`Prediction`} href="/prediction" />
-      <SidebarButton text={`Research\u00A0Portal`} href="/research" />
-    </div>
-  );
+	return (
+		<div className="bg-secondary-background p-2 pl-4 flex flex-col items-center gap-4 h-screen">
+			<Image src={logo} alt="StockSage" height={90} className="mb-10" />
+			<SidebarButton text="Dashboard" href="/dashboard" />
+			<SidebarButton text={`My\u00A0Stocks`} href="/myStocks" />
+			<SidebarButton text={`Prediction`} href="/prediction" />
+			<SidebarButton text={`Research\u00A0Portal`} href="/research" />
+		</div>
+	);
 };
 
 export default Sidebar;
