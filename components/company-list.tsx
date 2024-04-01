263ce2ad894cf7145da99d258bdad91a7d00845a
HarshitTib <harshittibrewal11@gmail.com>
2024-04-02 01:14:33 +0530

    [Fix: API error]

--- components/company-list.tsx
+++ components/company-list.tsx
@@ -3,181 +3,82 @@ import jsonFile from "../DataFiles/NSE.json";
 import { useEffect, useState } from "react";
 import { getMarketQuotes } from "../api/getMarketQuotes";
 
+interface Company {
+	segment: string;
+	name: string;
+	isin: string;
+	trading_symbol: string;
+}
+
 const CompanyList = ({ companyList }: { companyList: string[] }) => {
-	const [companyListDetails, setcompanyListDetails] = useState<
-		(
-			| { segment: string; name: string; isin: string; trading_symbol: string }
-			| undefined
-		)[]
-	>([]);
-
-	const findCompanyDetails = (
-		companyNames: string[]
-	): (
-		| { segment: string; name: string; isin: string; trading_symbol: string }
-		| undefined
-	)[] => {
-		const companyDetailsArray = companyNames.map((name) => {
-			const foundItem = jsonFile.find((item) => item.name === name);
-			return foundItem;
-		});
-		return companyDetailsArray.filter(Boolean);
-	};
+	const [companies, setCompanies] = useState<Company[]>([]);
+	const [marketQuotes, setMarketQuotes] = useState<any[]>([]);
 
 	useEffect(() => {
-		companyList &&
-			companyList.length > 0 &&
-			setcompanyListDetails(findCompanyDetails(companyList));
-	}, [companyList]);
+		const fetchCompanies = async () => {
+			const companiesData = companyList
+				.map((name) => {
+					const company = jsonFile.find((item) => item.name === name);
+					return company;
+				})
+				.filter(Boolean) as Company[];
 
-	console.log(companyListDetails);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await getMarketQuotes({ isin: "INE192A01025" }); 
-				console.log("Market quotes data:", data);
-			} catch (error) {
-				console.error("Error fetching market quotes:", error);
-			}
+			setCompanies(companiesData);
 		};
 
-		fetchData(); // Call the async function
-	}, []);
+		fetchCompanies();
+	}, [companyList]);
+
+	useEffect(() => {
+		const fetchMarketQuotesData = async () => {
+			const uniqueCompanies = companies.filter(
+				(company, index, self) =>
+					index === self.findIndex((c) => c.name === company.name)
+			);
+
+			const marketQuotesData = await Promise.all(
+				uniqueCompanies.map(async (company) => {
+					try {
+						const response = await getMarketQuotes({ isin: company.isin });
+						return { ...response, company }; // Combining response with company details
+					} catch (error) {
+						console.error("Error fetching market quotes:", error);
+						return null;
+					}
+				})
+			);
+
+			setMarketQuotes(marketQuotesData.filter(Boolean));
+		};
+
+		fetchMarketQuotesData();
+	}, [companies]);
 
 	return (
 		<div className="self-stretch overflow-hidden shrink-0 flex flex-col items-start justify-start text-left text-[0.75rem] text-darkslateblue-100 font-actor mq450:h-auto">
-			{companyListDetails.map((company) => {
-				//Error over here how to call the upstox api
-				// const companyDetails =
-				// 	company && getMarketQuotes({ isin: company.isin });
-				let companyDetails = null;
-				console.log("Company Details", company?.isin);
-				if (company) {
-					companyDetails = async () =>
-						await getMarketQuotes({ isin: company.isin });
-					let answer = companyDetails();
-					console.log("Company Details", answer);
-				}
-				return (
+			{marketQuotes.map((companyData, index) => (
+				<>
 					<WatchlistItem
-						cLogo="/amazonicon1-1.svg"
-						cName={company?.name}
-						cSymbol={company?.trading_symbol}
-						cValue="$102.24"
-						continueNode={+0.59}
+						key={index}
+						// cLogo="/amazonicon1-1.svg"
+						cName={companyData?.company?.name}
+						cSymbol={companyData?.company?.trading_symbol}
+						cValue={`₹${
+							companyData?.data?.[
+								`NSE_EQ:${companyData?.company?.trading_symbol}`
+							]?.last_price
+						}`}
+						continueNode={
+							companyData?.data?.[
+								`NSE_EQ:${companyData?.company?.trading_symbol}`
+							]?.net_change
+						}
 					/>
-				);
-			})}
-
-			<WatchlistItem
-				cLogo="/amazonicon1-1.svg"
-				cName="Amazon.com, Inc."
-				cSymbol="AMZN"
-				cValue="$102.24"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/cocacola6-1.svg"
-				cName="Coca-Cola Co"
-				cSymbol="KO"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$60.49"
-				continueNode={-0.32}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/bmw-1.svg"
-				cName="Bayerische Motoren Werke AG"
-				cSymbol="BMW"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$92.94"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/microsoft-1.svg"
-				cName="Microsoft Corp"
-				cSymbol="MSFT"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$248.16"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/ups-1.svg"
-				cName="United Parcel Service, Inc."
-				cSymbol="UPS"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$182.09"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/amazonicon1-1.svg"
-				cName="Amazon.com, Inc."
-				cSymbol="AMZN"
-				cValue="$102.24"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/cocacola6-1.svg"
-				cName="Coca-Cola Co"
-				cSymbol="KO"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$60.49"
-				continueNode={-0.32}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/bmw-1.svg"
-				cName="Bayerische Motoren Werke AG"
-				cSymbol="BMW"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$92.94"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/microsoft-1.svg"
-				cName="Microsoft Corp"
-				cSymbol="MSFT"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$248.16"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
-			<WatchlistItem
-				cLogo="/ups-1.svg"
-				cName="United Parcel Service, Inc."
-				cSymbol="UPS"
-				whileLoopNodeMarginTop="-0.687rem"
-				cValue="$182.09"
-				continueNode={+0.59}
-			/>
-			<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
-				<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
-			</div>
+					<div className="self-stretch h-[1.688rem] flex flex-row items-start justify-start pt-[0rem] px-[0rem] pb-[1.688rem] box-border mt-[-0.687rem]">
+						<div className="h-[0.063rem] flex-1 relative box-border border-t-[1px] border-solid border-royalblue-100" />
+					</div>
+				</>
+			))}
 		</div>
 	);
 };
