a986234a0f3975a9bc903aedf1fe8f7fe3102747
HarshitTib <harshittibrewal11@gmail.com>
2024-04-27 12:29:57 +0530

    Done with the prediction chart

--- components/company-list-container.tsx
+++ components/company-list-container.tsx
@@ -5,39 +5,40 @@ import "react-widgets/styles.css";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const CompanyListContainer = ({
-	setCompanyToDisplay,
+  setCompanyToDisplay,
 }: {
-	setCompanyToDisplay: Dispatch<SetStateAction<string>>;
+  setCompanyToDisplay: Dispatch<SetStateAction<string>>;
 }) => {
-	const [selectedCompanies, setSelectedCompanies] = useState<string[]>([]);
-	const [selectedCompany, setSelectedCompany] = useState("");
+  const [selectedCompanies, setSelectedCompanies] = useState<string[]>([
+    "TATA MOTORS LIMITED",
+  ]);
+  const [selectedCompany, setSelectedCompany] = useState("");
 
+  const handleSelect = (companyName: string) => {
+    setSelectedCompanies([...selectedCompanies, companyName]);
+    setSelectedCompany("");
+  };
 
-	const handleSelect = (companyName: string) => {
-		setSelectedCompanies([...selectedCompanies, companyName]);
-		setSelectedCompany("");
-	};
-
-	return (
-		<div className="self-stretch rounded-lg bg-whitesmoke flex flex-col items-start justify-start py-[1.063rem] pr-[0.719rem] pl-[0.75rem] gap-[1.125rem] text-center text-[0.875rem] text-darkslateblue-100 font-actor">
-			<div className="self-stretch flex flex-row items-start justify-between gap-[1.25rem]">
-				<Combobox
-					className="w-full"
-					hideCaret
-					hideEmptyPopup
-					data={jsonFile.map((company) => company.name)}
-					onChange={(companyName) => setSelectedCompany(companyName)}
-					onSelect={handleSelect}
-					placeholder="Add to your wishlist"
-					value={selectedCompany}
-				/>
-			</div>
-			<CompanyList
-				companyList={selectedCompanies}
-				setCompanyToDisplay={setCompanyToDisplay}
-			/>
-		</div>
-	);
+  return (
+    <div className="self-stretch rounded-lg bg-whitesmoke flex flex-col items-start justify-start py-[1.063rem] pr-[0.719rem] pl-[0.75rem] gap-[1.125rem] text-center text-[0.875rem] text-darkslateblue-100 font-actor">
+      <div className="self-stretch flex flex-row items-start justify-between gap-[1.25rem]">
+        <Combobox
+          className="w-full"
+          hideCaret
+          hideEmptyPopup
+          data={jsonFile.map((company) => company.name)}
+          onChange={(companyName) => setSelectedCompany(companyName)}
+          onSelect={handleSelect}
+          placeholder="Add to your wishlist"
+          value={selectedCompany}
+        />
+      </div>
+      <CompanyList
+        companyList={selectedCompanies}
+        setCompanyToDisplay={setCompanyToDisplay}
+      />
+    </div>
+  );
 };
 
 export default CompanyListContainer;
