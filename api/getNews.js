f4424a8b59ac2513a34f5c4d79c657cf2ecf5084
HarshitTib <harshittibrewal11@gmail.com>
2024-04-28 20:30:02 +0530

    Some changes

---
+++ api/getNews.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import React from "react";
+
+const getNews = async () => {
+  //   console.log(process.env.NEXT_PUBLIC_NEWS_API);
+  //   const res = await axios.get(
+  //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_NEWS_API}`
+  //   );
+  //   console.log(res);
+};
+
+export default getNews;
--- pages/news-page.tsx
+++ pages/news-page.tsx
@@ -3,8 +3,14 @@ import FrameComponent3 from "../components/frame-component3";
 import FrameComponent2 from "../components/frame-component2";
 import FrameWithText from "../components/frame-with-text";
 import Navbar from "../components/navbar";
+import { useEffect } from "react";
+import getNews from "../api/getNews";
 
 const NewsPage: NextPage = () => {
+  useEffect(() => {
+    getNews();
+  }, []);
+
   return (
     <div className="w-full relative rounded-13xl bg-whitesmoke-100 overflow-hidden flex flex-col items-end justify-start pt-[0.688rem] pb-[16.938rem] pr-[4.75rem] pl-[0rem] box-border gap-[4rem] tracking-[normal] mq975:gap-[2rem_4rem] mq975:pr-[2.375rem] mq975:box-border mq725:gap-[1rem_4rem]">
       <Navbar />
