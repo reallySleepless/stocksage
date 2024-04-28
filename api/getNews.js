3dae115b3141b095d06f09a1618092519f3de507
HarshitTib <harshittibrewal11@gmail.com>
2024-04-28 21:41:44 +0530

    Completed with the news api

--- api/getNews.js
+++ api/getNews.js
@@ -2,11 +2,20 @@ import axios from "axios";
 import React from "react";
 
 const getNews = async () => {
-  //   console.log(process.env.NEXT_PUBLIC_NEWS_API);
-  //   const res = await axios.get(
-  //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_NEWS_API}`
-  //   );
-  //   console.log(res);
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API;
+  const apiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${apiKey}`;
+  const res = await axios.get(apiUrl);
+  // console.log(res.data);
+  return res.data;
+  // axios
+  //   .get(apiUrl)
+  //   .then((response) => {
+  //     // Process the data here
+  //     console.log(response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("There was a problem fetching the data:", error);
+  //   });
 };
 
 export default getNews;
