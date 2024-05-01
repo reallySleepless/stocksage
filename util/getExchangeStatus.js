fbae9bd6c543e6d37a7082b05976b0056286aff5
Himanshu Srivastava <himanshu20032002@gmail.com>
2024-05-01 17:17:52 +0530

    test3

---
+++ util/getExchangeStatus.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+import Router from "next/router";
+
+export const getExchangeStatus = async ({ exchange }) => {
+  const token = localStorage.getItem("upstoxToken");
+  const url = `https://api.upstox.com/v2/market/status/${exchange}`;
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+  try {
+    const response = await axios.get(url, { headers });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    Router.push("/login");
+  }
+};
---
+++ util/getMetaData.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+export const getMetaData = async () => {
+	const url = "https://api.upstox.com/v2/trade/profit-loss/metadata";
+	const token = localStorage.getItem("upstoxToken");
+	const headers = {
+		Accept: "application/json",
+		Authorization: `Bearer ${token}`,
+	};
+
+	const params = {
+		from_date: "05-11-2023",
+		to_date: "19-12-2023",
+		segment: "EQ",
+		financial_year: "2324",
+	};
+
+	axios
+		.get(url, { headers, params })
+		.then((response) => {
+			console.log(response.status);
+			console.log(response.data);
+		})
+		.catch((error) => {
+			console.error("Error:", error.message || error);
+		});
+};
